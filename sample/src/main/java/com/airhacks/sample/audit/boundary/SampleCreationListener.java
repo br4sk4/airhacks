fc94728973b5e6ceb28098d01d1a46514480830b
Adam Bien <abien@adam-bien.com>
2017-03-07 13:39:20 +0100

    CDI Event listener are executed synchronously

--- sample/src/main/java/com/airhacks/sample/audit/boundary/SampleCreationListener.java
+++ sample/src/main/java/com/airhacks/sample/audit/boundary/SampleCreationListener.java
@@ -1,6 +1,8 @@
 
 package com.airhacks.sample.audit.boundary;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.event.Observes;
 import javax.enterprise.event.TransactionPhase;
 import javax.json.JsonObject;
@@ -18,6 +20,11 @@ public class SampleCreationListener {
 
     public void onFailedAttempt(@Observes(during = TransactionPhase.AFTER_FAILURE) JsonObject sample) {
         System.out.println("---audit- creation failed: " + sample);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SampleCreationListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
