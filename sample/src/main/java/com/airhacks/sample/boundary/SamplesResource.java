2c47383f4207c4807e1ff18b72497915d58ed20c
Adam Bien <abien@adam-bien.com>
2017-03-07 10:28:36 +0100

    DI in Java EE is working

--- sample/src/main/java/com/airhacks/sample/boundary/SamplesResource.java
+++ sample/src/main/java/com/airhacks/sample/boundary/SamplesResource.java
@@ -1,15 +1,19 @@
 
 package com.airhacks.sample.boundary;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("samples")
 public class SamplesResource {
 
+    @Inject
+    SamplesSupplier supplier;
+
     @GET
     public String hello() {
-        return "hey joe";
+        return supplier.hey();
     }
 
 
---
+++ sample/src/main/java/com/airhacks/sample/boundary/SamplesSupplier.java
@@ -0,0 +1,15 @@
+
+package com.airhacks.sample.boundary;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class SamplesSupplier {
+
+    public String hey() {
+        return "hello " + System.currentTimeMillis();
+    }
+
+
+}
